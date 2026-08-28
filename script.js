b3aa74e5b7526d3588cd60b1ecb04c842f2a2c9b
vivi20102009 <maria.silva.fernandes20@escola.pr.gov.br>
2026-08-28 09:10:35 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(". texto-resultado");
+
+const perguntas = [
+    {
+    enunciado:"Pergunta 1",
+    alternativas: [
+        "Alternativa1",
+        "alternativa2"
+        
+    ]
+},
+{
+    enunciado:"Pergunta 2",
+    alternativas: [
+        "Alternativa1",
+        "Alternativa2"
+        
+    ]
+}
+    ];
